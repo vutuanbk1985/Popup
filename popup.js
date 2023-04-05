7f7afe408b74b9218a261de25b3fd90304adc65c
Tuan Vu <117656833+vutuanbk1985@users.noreply.github.com>
2023-04-05 08:58:36 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -7,7 +7,7 @@ const body = document.getElementsByTagName('body');
 
 function show()    {
     popup.classList.toggle('visibility-toggle');
-    alert('Hello');
+    
 }
 
 
@@ -20,10 +20,10 @@ window.addEventListener('load', show);
 // window.addEventListener('load', setTimeout(show, 3000));
 
 closebtn.addEventListener('click', () => {
-    // popup.classList.remove('visibility-toggle');
-    // popup.classList.toggle('invisibility-toggle');
-    popup.classList.toggle('visibility-toggle');
-    // alert('Good bye');
+    popup.classList.remove('visibility-toggle');
+    popup.classList.toggle('invisibility-toggle');
+    
+    
 })
 
 
